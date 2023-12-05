cf549b117e1eaf8c374c9bf6edba1355fb4d7608
Piltxi <eliapitzalis@hotmail.com>
2023-12-05 20:28:25 +0100

    dataset loading and export module

--- datasetLoad.py
+++ datasetLoad.py
@@ -1,65 +1,52 @@
-import csv
+import pandas as pd
 import argparse
 import subprocess
 import os
 
-def copyCSV (inputFile, outputFile):
-    
-    try:
+def resetDataset():
         
-        with open(inputFile, 'r', encoding='utf-8') as source_file:
-            reader = csv.reader(source_file)
-            data = [row for row in reader]
-
-        with open(outputFile, 'w', encoding='utf-8', newline='') as destination_file:
-            writer = csv.writer(destination_file)
-            writer.writerows(data)
-    
-    except FileNotFoundError:
-        print(f"ERROR] '{inputFile}' -input file not found")
-        quit()
-    
-    except Exception as e:
-        print(f"ERROR] '{inputFile}' -during copying {e}")
-        quit()
-
-if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser(description="WineTz v.1")
-    parser.add_argument("-r", "--reset", action="store_true", help="reset directory dataset/")
-
-    args = parser.parse_args()
-
-    if (args.reset): 
-        
-        if os.path.exists("dataset/"):
-            try:
-                subprocess.run(["rm", "-fr", "dataset"], check=True)
-            except subprocess.CalledProcessError as e:
-                print(f"Error in removing the existing directory: {e}")
-
-        print ("reset dataset folder")
-        quit()
-
-    #* path directory definition
-    directory_name = "dataset/"
-
-    if not os.path.exists(directory_name):
+    if os.path.exists("dataset/"):
         try:
-            os.makedirs(directory_name)
+            subprocess.run(["rm", "-fr", "dataset"], check=True)
         except subprocess.CalledProcessError as e:
-            print(f"Error in creating the directory: {e}")
+            print(f"Error in removing the existing directory: {e}")
 
-    inputFile_path = 'crawler/out/out 04.12/dataset 17.56/reviews.csv'
-    outputFile_path = 'dataset/reviews.csv'
-    copyCSV (inputFile_path, outputFile_path)
+    print ("reset dataset folder")
+    quit()
 
-    inputFile_path = 'crawler/out/out 04.12/dataset 17.56/wine.csv'
-    outputFile_path = 'dataset/wine.csv'
-    copyCSV (inputFile_path, outputFile_path)
 
-    inputFile_path = 'crawler/out/out 04.12/dataset 17.56/style.csv'
-    outputFile_path = 'dataset/style.csv'
-    copyCSV (inputFile_path, outputFile_path)
+parser = argparse.ArgumentParser(description="WineTz v.1")
+parser.add_argument("-r", "--reset", action="store_true", help="reset directory dataset/")
+parser.add_argument("-f", "--file", action="store_true", help="specify output /path/")
 
-    print(f"dataset copied successfully")
+args = parser.parse_args()
+
+if args.reset: 
+    resetDataset()
+
+inputPath = input ("Type path directory of .csv from crawler or other> ")
+
+#* path directory definition
+directory_name = "dataset/"
+
+if args.file:
+    directory_name = input ("Type path directory of final dataset> ")
+
+if not os.path.exists(directory_name):
+    try:
+        os.makedirs(directory_name)
+    except subprocess.CalledProcessError as e:
+        print(f"Error in creating the directory: {e}")
+
+reviews_df = pd.read_csv(f'{inputPath}/reviews.csv')
+wine_df = pd.read_csv(f'{inputPath}/wine.csv')
+style_df = pd.read_csv(f'{inputPath}/style.csv')
+
+merged_df = pd.merge(reviews_df, wine_df, left_on='idWine', right_on='ID', how='inner')
+merged_df = pd.merge(merged_df, style_df, left_on='Style', right_on='ID', how='inner')
+selected_columns = ['idRev', 'Language', 'Winery', 'Name', 'Year', 'Region', 'Description', 'Nation', 'User Rating', 'Note', 'CreatedAt', 'Rating', 'Rates count', 'Type', 'Price']
+result_df = merged_df[selected_columns]
+
+result_df.to_csv(f'{directory_name}/dataset.csv', index=False, encoding='utf-8')
+
+print("Merge and selection completed successfully. Results exported in 'dataset/dataset.csv'.")
