6180c0dc5d5b0ced5a0e208768bd340d07e9f764
Piltxi <eliapitzalis@hotmail.com>
2024-01-19 20:03:53 +0100

    benchmarks tec in testing

--- benchmark/benchmarks.py
+++ benchmark/benchmarks.py
@@ -1,3 +1,4 @@
+import json
 import math
 import sys
 
@@ -43,101 +44,111 @@ def ndcg(scores):
     
     return round(num/dcg(scores)[lenght], 3)
 
-def applyRelevance (rObject, results): 
+def applyRelevance (nlQuery, rObject, results): 
 
-    print ("Query inserted: ", rObject)
+    print (nlQuery, "\n", rObject)
+
+    resultMod = results [:10] if len(results) > 10 else results
 
     relevance = []
-    for result in results: 
+    for result in resultMod: 
         print (resultFormatter(result))
         relevance.append(int(input("Insert relevance about this result: ")))
 
     return relevance
 
-def benchmarksQueries(ix): 
+def getBenchmarks (allQuery): 
 
-    # About "valpolicella" wine
+    allBenchmarks = []
+
+    nlQuery, queryText, parameters = allQuery[4]
+    rObject, results = queryReply(ix, parameters, queryText)
+    relevance = applyRelevance (nlQuery, rObject, results)
+
+    DCG_banch = dcg(relevance)
+    NDCG_banch = ndcg(relevance)
+
+    allBenchmarks.append ([nlQuery, relevance, DCG_banch, NDCG_banch])
+
+    print("DCG values for first 10 document retrieved:\n", DCG_banch)
+    print("Normalized DCG for 10 retrieved documents:\n", NDCG_banch)
+
+    return allBenchmarks
+
+def setBenchmarksQueries(ix): 
+
+    allQuery = []
+
+    nlQuery = "Query n1: 'Vino Valpolicella'"
     queryText = "Valpolicella"
     parameters = [False, ["wine_name"], None, ["1"], (None), False, False, False, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject)
+    allQuery.append([nlQuery, queryText, parameters])
     
-    # About "Valdo" winery
+    nlQuery = "Query n2: 'Vini cantina Valdo'"
     queryText = "Valdo"
     parameters = [False, ["wine_winery"], None, None, (None), False, False, False, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
-    # About "Montepulciano" wine, high->joy sentiment requests, 10:20 price interval
+    nlQuery = "Query n3: 'Vino Montepulciano, impressione positiva, Prezzo compreso tra 10 e 20€'"
     queryText = "Montepulciano"
     parameters = [False, ["wine_name"], [(10), (20)], ["1"], (["M", "joy"]), False, False, False, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
-    # About white wine, meat and fish, high->joy sentiment requests
+    nlQuery = "Query n4: 'Vini bianchi adatti sia per la carne che per il pesce, recensioni positive, Prezzo compreso tra 20 e 30€'"
     queryText = "carne e pesce"
     parameters = [False, ["review_note"], [(20), (30)], ["1", "2"], (["M", "joy"]), False, False, True, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
-    # About white wine, meat and fish, high->joy sentiment requests
-    queryText = "carne e pesce"
-    parameters = [False, ["review_note"], [(20), (30)], ["1", "2"], (["M", "joy"]), False, False, True, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
-
-    # About steak [without and with th.extension]
+    nlQuery = "Query n5: 'Vini da abbinare a bistecche [con e senza thesaurus]'"
     queryText = "bistecca"
     parameters = [False, ["review_note"], None, None, (["M", "joy"]), False, False, False, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
     queryText = "bistecca"
     parameters = [False, ["review_note"], None, None, (["M", "joy"]), False, True, False, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
-    # About cheap wines, high->sadness sentiment requests
+    nlQuery = "Query n6: ' Vini economici ma con recensioni tristi'"
     queryText = "economico"
     parameters = [False, ["review_note"], None, None, (["M", "sadness"]), False, False, False, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
-    # About wines to celebrate, high->joy sentiment requests, price 0:20 [without and with th.extension]
+    nlQuery = "Query n7: 'Vini per festeggiare, sotto i 20€'"
     queryText = "festa"
     parameters = [False, ["review_note"], [(None), (20)], None, (["M", "joy"]), False, False, False, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
     queryText = "festa"
-    parameters = [False, ["review_note"], [(None), (20)], None, (["M", "joy"]), False, False, True, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    parameters = [False, ["review_note"], [(None), (20)], None, (["M", "joy"]), False, True, False, False, None]
+    allQuery.append([nlQuery, queryText, parameters])
 
-    # About wines with great personality, year 2012
+    nlQuery = "Query n8: 'Vini di grande personalità del 2012'"
     queryText = "grande personalità"
     parameters = [False, ["review_note"], None, None, (["M", "joy"]), False, False, True, False, 2012]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
-    # About fortified wines with hints of honey
+    nlQuery = "Query n9: 'Vini Liquorosi con sentori di miele, recensioni positive'"
     queryText = "miele"
     parameters = [False, ["review_note"], None, ["24"], None, False, False, True, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
-    # About 'lambrusco' and 'gnocco fritto': two typical delicacies of Emilia Romagna, Italy
+    nlQuery = "Query n10: 'Lambrusco con gnocco fritto'"
     queryText = "lambrusco e gnocco fritto"
     parameters = [False, ["wine_name", "review_note"], None, None, None, False, False, True, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
 
     queryText = " > wine_name:lambrusc AND review_note:gnocc AND review_note:fritt"
     parameters = [False, ["wine_name", "review_note"], None, None, None, False, False, True, False, None]
-    rObject, results = queryReply(ix, parameters, queryText)
-    print (rObject) 
+    allQuery.append([nlQuery, queryText, parameters])
+
+    return allQuery
 
 if __name__ == '__main__':
 
     ix = loadIndex(GUI=False, rebooting=False)
-    benchmarksQueries (ix)
+    
+    allQuery = setBenchmarksQueries (ix)
+    allBenchmarks = getBenchmarks(allQuery)
+    with open('benchmarks_data5.json', 'w') as json_file:
+         json.dump(allBenchmarks, json_file, indent=2)
+
