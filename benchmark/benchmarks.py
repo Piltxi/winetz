370676edd79e19180c983bd6ac72e35ca63855d3
Piltxi <eliapitzalis@hotmail.com>
2024-01-21 20:09:15 +0100

    add some comments

--- benchmark/benchmarks.py
+++ benchmark/benchmarks.py
@@ -3,14 +3,13 @@ import math
 import sys
 
 sys.path.append('../searcher')
-from searcherIO import loadIndex, queryReply, printResultsCLI, resultFormatter
+from searcherIO import loadIndex, queryReply, resultFormatter
 
 '''
     code to add search parameters 
 
-    UIMode, searchField, priceInterval, wineType, sentimentRequest, algorithm, thesaurusFlag, andFlag, correctionFlag, year = parameters
-
     UIMode = False
+    UIMode, searchField, priceInterval, wineType, sentimentRequest, algorithm, thesaurusFlag, andFlag, correctionFlag, year = parameters
 
     searchField = ["wine_name", "review_note", "wine_winery"]
     priceInterval = [(None), (10)]
@@ -22,21 +21,37 @@ from searcherIO import loadIndex, queryReply, printResultsCLI, resultFormatter
     correctionFlag = False
     year = None
 
+    the following lines are examples of parameters specification in code:
+
     parameters = UIMode searchField                                   priceInterval    wineType     sentimentRequest    algorithm,  thesaurusFlag   andFlag     correctionFlag  year
     parameters = [False, ["wine_name", "review_note", "wine_winery"], [(None), (20)], ["1", "2"],   (["M", "joy"]),     False,      False,          False,       False,           None]
 '''
 
 def dcg (scores):
-    gains=[scores[0]]
-    dg=[scores[0]]
-
-    for s in range(1, len(scores)):
-        dg.append(scores[s]/math.log(1+s, 2))
-        gains.append(round(gains[s-1]+dg[s], 3))
     
+    """ Discounted cumulative gain calcula
+
+    Returns:
+        gains: list with gains calculated by user-assigned relevances
+    """
+
+    gains = [scores[0]]
+    dg = [scores[0]]
+
+    for s in range (1, len(scores)):
+        dg.append(scores[s] / math.log (1+s, 2))
+        gains.append(round(gains[s-1]+dg[s], 3))
+
     return gains
 
 def ndcg(scores):
+    
+    """ Normalized discounted cumulative gain calcula
+
+    Returns:
+        gains: value calculated from Discounted cumulative gain calcula
+    """
+
     lenght = len(scores)-1
     num = dcg(scores)[lenght]
     
@@ -45,6 +60,20 @@ def ndcg(scores):
     return round(num/dcg(scores)[lenght], 3)
 
 def applyRelevance (nlQuery, rObject, results): 
+
+    """ method to set relevance about result obtained with query
+
+        applyRelevance works on a reduce numbers of obtained result
+        is normally used to apply the relevance of the results obtained from different search algorithms:
+        the number of results is reduced to 10.
+
+    nlQuery: query in natural language
+    rObject: query received by search engine module
+    results: list of obtained result 
+
+    Returns:
+        relevance: list with user-assigned relevances
+    """
 
     print (nlQuery, "\n", rObject)
 
@@ -58,6 +87,16 @@ def applyRelevance (nlQuery, rObject, results):
     return relevance
 
 def getBenchmarks (ix, allQuery): 
+
+    """ getBenchmarks works with queries list 
+        - getBenchmarks show results for every query and it allows user to set related relevance
+
+        ix: index data object loaded
+        allQuery: list with queries [defined in setBenchmarksQueries()]
+
+    Returns:
+        allBenchmarks: list with all queries and related relevance, DCG and NDCG of obteined results
+    """
 
     allBenchmarks = []
 
@@ -79,7 +118,19 @@ def getBenchmarks (ix, allQuery):
 
 def setBenchmarksQueries(): 
 
+    '''
+        definition query list and related search filters
+        
+        Returns:
+            allQuery: list with loaded queries
+    '''
+
     allQuery = []
+
+    nlQuery = "Query n1: 'Vino Valpolicella'"
+    queryText = "Valpolicella"
+    parameters = [False, ["wine_name"], None, ["1"], (None), False, False, False, False, None]
+    allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n1: 'Vino Valpolicella'"
     queryText = "Valpolicella"
@@ -88,59 +139,59 @@ def setBenchmarksQueries():
     
     nlQuery = "Query n2: 'Vini cantina Valdo'"
     queryText = "Valdo"
-    parameters = [False, ["wine_winery"], None, None, (None), True, False, False, False, None]
+    parameters = [False, ["wine_winery"], None, None, (None), False, False, False, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n3: 'Vino Montepulciano, impressione positiva, Prezzo compreso tra 10 e 20€'"
     queryText = "Montepulciano"
-    parameters = [False, ["wine_name"], [(10), (20)], ["1"], (["M", "joy"]), True, False, False, False, None]
+    parameters = [False, ["wine_name"], [(10), (20)], ["1"], (["M", "joy"]), False, False, False, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n4: 'Vini bianchi adatti sia per la carne che per il pesce, recensioni positive, Prezzo compreso tra 20 e 30€'"
     queryText = "carne e pesce"
-    parameters = [False, ["review_note"], [(20), (30)], ["1", "2"], (["M", "joy"]), True, False, True, False, None]
+    parameters = [False, ["review_note"], [(20), (30)], ["1", "2"], (["M", "joy"]), False, False, True, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n5: 'Vini da abbinare a bistecche [con e senza thesaurus]'"
     queryText = "bistecca"
-    parameters = [False, ["review_note"], None, None, (["M", "joy"]), True, False, False, False, None]
+    parameters = [False, ["review_note"], None, None, (["M", "joy"]), False, False, False, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     queryText = "bistecca"
-    parameters = [False, ["review_note"], None, None, (["M", "joy"]), True, True, False, False, None]
+    parameters = [False, ["review_note"], None, None, (["M", "joy"]), False, True, False, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n6: ' Vini economici ma con recensioni tristi'"
     queryText = "economico"
-    parameters = [False, ["review_note"], None, None, (["M", "sadness"]), True, False, False, False, None]
+    parameters = [False, ["review_note"], None, None, (["M", "sadness"]), False, False, False, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n7: 'Vini per festeggiare, sotto i 20€'"
     queryText = "festa"
-    parameters = [False, ["review_note"], [(None), (20)], None, (["M", "joy"]), True, False, False, False, None]
+    parameters = [False, ["review_note"], [(None), (20)], None, (["M", "joy"]), False, False, False, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     queryText = "festa"
-    parameters = [False, ["review_note"], [(None), (20)], None, (["M", "joy"]), True, True, False, False, None]
+    parameters = [False, ["review_note"], [(None), (20)], None, (["M", "joy"]), False, True, False, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n8: 'Vini di grande personalità del 2012'"
     queryText = "grande personalità"
-    parameters = [False, ["review_note"], None, None, (["M", "joy"]), True, False, True, False, 2012]
+    parameters = [False, ["review_note"], None, None, (["M", "joy"]), False, False, True, False, 2012]
     allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n9: 'Vini Liquorosi con sentori di miele, recensioni positive'"
     queryText = "miele"
-    parameters = [False, ["review_note"], None, ["24"], None, True, False, True, False, None]
+    parameters = [False, ["review_note"], None, ["24"], None, False, False, True, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     nlQuery = "Query n10: 'Lambrusco con gnocco fritto'"
     queryText = "lambrusco e gnocco fritto"
-    parameters = [False, ["wine_name", "review_note"], None, None, None, True, False, True, False, None]
+    parameters = [False, ["wine_name", "review_note"], None, None, None, False, False, True, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     queryText = " > wine_name:lambrusc AND review_note:gnocc AND review_note:fritt"
-    parameters = [False, ["wine_name", "review_note"], None, None, None, True, False, True, False, None]
+    parameters = [False, ["wine_name", "review_note"], None, None, None, False, False, True, False, None]
     allQuery.append([nlQuery, queryText, parameters])
 
     return allQuery
@@ -150,7 +201,9 @@ if __name__ == '__main__':
     ix = loadIndex(GUI=False, rebooting=False)
     
     allQuery = setBenchmarksQueries ()
+    
     allBenchmarks = getBenchmarks(ix, allQuery)
     
-    with open('benchmarks_data25.json', 'w') as json_file:
+    # export measurements obtained with corrispondent queries
+    with open('benchmarks.json', 'w') as json_file:
         json.dump(allBenchmarks, json_file, indent=2)
